2b0d520951f75928f96576a3e2952909058e4be4
Brian_H <hurleyb5@wit.edu>
2020-06-06 14:38:01 -0400

    Able to import node.js modules & print temp dir path

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,10 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+//START TESTING
+import * as testing from './temp_dir';
+//END TESTING
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -9,6 +13,15 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Archive Browser activated.');
+
+	//START TESTING
+	console.log('Start testing');
+	
+	//import * as testing from './temp_dir';
+	testing.create_temp_dir();
+
+	console.log('End testing');
+	//END TESTING
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
--- src/temp_dir.ts
+++ src/temp_dir.ts
@@ -1,4 +1,26 @@
 //TODO: Document code
 import * as vscode from 'vscode';
+import * as os from 'os';
+import * as fs from 'fs';
 
-//Type
+//Functions
+
+//TODO: Complete function
+export function create_temp_dir(){
+    let default_temp_dir: string = os.tmpdir();
+
+    console.log(default_temp_dir);
+
+    return 0;
+}
+
+//This function might not be needed. 
+function get_os(){
+    let OS_Type: string = "Unknown";
+
+    OS_Type = "New String";
+
+    OS_Type = os.type();
+
+    return OS_Type;
+}
